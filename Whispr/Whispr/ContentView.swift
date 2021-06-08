d27984b2ba0fe4bf883b6c5831d0511a11a9c50d
Paul Erny <paul.erny@epitech.eu>
2021-06-08 20:00:03 +0200

    ADD - app setup

---
+++ Whispr/Whispr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Whispr
+//
+//  Created by Paul Erny on 08/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Whispr/Whispr/WhisprApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisprApp.swift
+//  Whispr
+//
+//  Created by Paul Erny on 08/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct WhisprApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
