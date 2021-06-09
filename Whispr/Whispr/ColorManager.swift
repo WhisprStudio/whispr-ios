6bc68a0c6d03322b96f6d6d44e6b8907953f1a53
PaulErny <paul.erny@epitech.eu>
2021-06-09 20:00:39 +0200

    ADD - Custom font manager #3

--- Whispr/Whispr/ColorManager.swift
+++ Whispr/Whispr/ColorManager.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 extension Color {
     static let background = Color("Background")
-    static let diabledText = Color("DisabledText")
-    static let diabledField = Color("DisabledField")
+    static let disabledText = Color("DisabledText")
+    static let disabledField = Color("DisabledField")
     static let error = Color("Error")
     static let primaryText = Color("PrimaryText")
     static let LED = Color("LED")
