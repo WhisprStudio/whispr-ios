2be1ddf6a185c000656d3e3520020fbd2cdc1422
PaulErny <paul.erny@epitech.eu>
2021-06-09 18:16:44 +0200

    ADD - Color management system #2

---
+++ Whispr/Whispr/ColorManager.swift
@@ -0,0 +1,18 @@
+//
+//  ColorManager.swift
+//  Whispr
+//
+//  Created by Paul Erny on 09/06/2021.
+//
+
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let diabledText = Color("DisabledText")
+    static let diabledField = Color("DisabledField")
+    static let error = Color("Error")
+    static let primaryText = Color("PrimaryText")
+    static let LED = Color("LED")
+    static let success = Color("Success")
+}
--- Whispr/Whispr/ContentView.swift
+++ Whispr/Whispr/ContentView.swift
@@ -11,11 +11,16 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .foregroundColor(.success)
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            .background(Color.background)
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
