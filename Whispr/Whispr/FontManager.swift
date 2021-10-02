70d6ddc5350fb80698587367bdcbfbe011b7debf
PaulErny <paul.erny@epitech.eu>
2021-10-02 13:47:58 +0100

    ADD - customizable fonts outside of the package #18

--- Whispr/Whispr/FontManager.swift
+++ Whispr/Whispr/FontManager.swift
@@ -5,37 +5,20 @@
 //  Created by Paul Erny on 09/06/2021.
 //
 
+import WhisprDesignable
 import SwiftUI
 
 //MARK: - SwiftUI
-struct BarlowFontModifier: ViewModifier {
+struct PrimaryFontModifier: ViewModifier {
     var weight: FontWeight = .medium
     var size: FontSize = .XS
-    var modifier: FontModifier = .none
+    var modifier: FontType = .none
     
     func body(content: Content) -> some View {
-        let fontAttribute = weight == .thin ? weight.rawValue + FontModifier.italic.rawValue : weight.rawValue
+        let fontAttribute = weight == .thin ? weight.rawValue + FontType.italic.rawValue : weight.rawValue
         return content
             .font(.custom("Barlow-\(fontAttribute)", size: size.rawValue))
             .foregroundColor(.primaryText)
     }
     
-}
-
-public enum FontWeight: String {
-    case regular = "Regular", medium = "Medium", thin = "Thin"
-}
-
-public enum FontModifier: String {
-    case none = "", italic = "Italic"
-}
-
-public enum FontSize: CGFloat {
-    case XXL = 45, XL = 36, L = 28, M = 20, S = 17, XS = 15, XXS = 13, XXXS = 11
-}
-
-extension View {
-    func primaryFont(size: FontSize, weight: FontWeight) -> some View {
-        self.modifier(BarlowFontModifier(weight: weight, size: size))
-    }
 }
--- WhisprDesignable/Sources/WhisprDesignable/FontManager.swift
+++ WhisprDesignable/Sources/WhisprDesignable/FontManager.swift
@@ -8,34 +8,59 @@
 import SwiftUI
 
 //MARK: - SwiftUI
-struct BarlowFontModifier: ViewModifier {
-    var weight: FontWeight = .medium
-    var size: FontSize = .XS
-    var modifier: FontModifier = .none
-    
-    func body(content: Content) -> some View {
-        let fontAttribute = weight == .thin ? weight.rawValue + FontModifier.italic.rawValue : weight.rawValue
-        return content
-            .font(.custom("Barlow-\(fontAttribute)", size: size.rawValue))
-            .foregroundColor(.primaryText)
-    }
-    
+public protocol FontModifier: ViewModifier {
+    var weight: FontWeight { get set }
+    var size: FontSize { get set }
+    var modifier: FontType { get set }
 }
 
 public enum FontWeight: String {
     case regular = "Regular", medium = "Medium", thin = "Thin"
 }
 
-public enum FontModifier: String {
-    case none = "", italic = "Italic"
+// TODO: add more ? (semi bold, extra bold, bold italic, ...)
+public enum FontType: String {
+    case none = "", italic = "Italic", bold = "Bold"
 }
 
 public enum FontSize: CGFloat {
     case XXL = 45, XL = 36, L = 28, M = 20, S = 17, XS = 15, XXS = 13, XXXS = 11
 }
 
-extension View {
+// MARK: for demonstrative purpose
+// TODO: import barlow to the package for this to work
+struct PrimaryFontModifier: FontModifier {
+    var weight: FontWeight = .medium
+    var size: FontSize = .XS
+    var modifier: FontType = .none
+    
+    func body(content: Content) -> some View {
+//        let fontAttribute = weight == .thin ? weight.rawValue + FontType.italic.rawValue : weight.rawValue
+        return content
+            .font(.system(size: size.rawValue, weight: .medium, design: .default))
+//            .font(.custom("Barlow-\(fontAttribute)", size: size.rawValue))
+            .foregroundColor(.primaryText)
+    }
+}
+
+struct SecondaryFontModifier: FontModifier {
+    var weight: FontWeight = .medium
+    var size: FontSize = .XS
+    var modifier: FontType = .none
+    
+    func body(content: Content) -> some View {
+        return content
+            .font(.system(size: size.rawValue, weight: .medium, design: .default))
+            .foregroundColor(.primaryText)
+    }
+}
+
+public extension View {
     func primaryFont(size: FontSize, weight: FontWeight) -> some View {
-        self.modifier(BarlowFontModifier(weight: weight, size: size))
+        self.modifier(PrimaryFontModifier(weight: weight, size: size))
+    }
+    
+    func secondaryFont(size: FontSize, weight: FontWeight) -> some View {
+        self.modifier(SecondaryFontModifier(weight: weight, size: size))
     }
 }
