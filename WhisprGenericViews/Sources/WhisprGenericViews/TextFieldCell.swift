b036d456bdb3c62c5bbe6e1bdfeb8ee5566e9794
PaulErny <paul.erny@epitech.eu>
2021-10-04 16:20:07 +0100

    FIX - fix preview error in local package v2

---
+++ WhisprGenericViews/Sources/WhisprGenericViews/TextFieldCell.swift
@@ -0,0 +1,21 @@
+//
+//  TextFieldCell.swift
+//  
+//
+//  Created by Paul Erny on 04/10/2021.
+//
+
+import SwiftUI
+
+public struct TextFieldCell: View {
+    public var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct TextFieldCell_Previews: PreviewProvider {
+    static var previews: some View {
+        TextFieldCell()
+            .preferredColorScheme(.dark)
+    }
+}
