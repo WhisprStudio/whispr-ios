6e3bf603002c6ab556ddd5d23c9b71d5767a405f
PaulErny <paul.erny@epitech.eu>
2021-10-15 15:09:05 +0100

    FIX - added white mode colors to the package #16

--- WhisprGenericViews/Sources/WhisprGenericViews/ColorManager.swift
+++ WhisprGenericViews/Sources/WhisprGenericViews/ColorManager.swift
@@ -8,23 +8,13 @@
 import SwiftUI
 
 extension Color {
-//    static let background = Color("Background")
-//    static let disabledText = Color("DisabledText")
-//    static let disabledField = Color("DisabledField")
-//    static let error = Color("Error")
-//    static let primaryText = Color("PrimaryText")
-//    static let LED = Color("LED")
-//    static let success = Color("Success")
-//    static let FieldBackground = Color("FieldBackground")
-//    static let WhisprYellow = Color("WhisprYellow")
-    
-    static let background = Color(red: 0.145, green: 0.145, blue: 0.176)
-    static let disabledText = Color(red: 0.757, green: 0.757, blue: 0.757)
-    static let disabledField = Color(red: 0.718, green: 0.706, blue: 0.647)
-    static let error = Color(red: 0.898, green: 0.357, blue: 0.357)
-    static let primaryText = Color(red: 0.686, green: 0.686, blue: 0.686)
-    static let LED = Color(red: 0.161, green: 0.529, blue: 0.184)
-    static let success = Color(red: 1.0, green: 0.835, blue: 0.0)
-    static let FieldBackground = Color(red: 0.184, green: 0.184, blue: 0.216)
-    static let WhisprYellow = Color(red: 1.0, green: 0.835, blue: 0.0)
+    static let background = Color("Background")
+    static let disabledText = Color("DisabledText")
+    static let disabledField = Color("DisabledField")
+    static let error = Color("Error")
+    static let primaryText = Color("PrimaryText")
+    static let LED = Color("LED")
+    static let success = Color("Success")
+    static let FieldBackground = Color("FieldBackground")
+    static let WhisprYellow = Color("WhisprYellow")
 }
