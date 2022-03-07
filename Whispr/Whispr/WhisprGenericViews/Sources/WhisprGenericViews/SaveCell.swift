f060bb3330adbf6f525fb8f416ab6e231defb569
PaulErny <paul.erny@epitech.eu>
2022-03-07 17:26:41 +0000

    ADD - add documentation to SaveCell

--- Whispr/Whispr/WhisprGenericViews/Sources/WhisprGenericViews/SaveCell.swift
+++ Whispr/Whispr/WhisprGenericViews/Sources/WhisprGenericViews/SaveCell.swift
@@ -7,11 +7,44 @@
 
 import SwiftUI
 
+/// A control that initiates an action.
+///
+/// A premade customization of the ``ButtonCell`` specifically dedicated to validating an action.
+/// The label is preset to "Save" and its color is set to iOS's default blue by default.
+///
+/// Therefore it only takes a method or closure property as parameter. For example:
+///
+/// ```swift
+///    SaveCell(action: saveContent)
+/// ```
+///
+/// ### Styling DeleteCell
+///
+/// ```swift
+///    SaveCell(action: saveContent)
+///        .saveTextColor(.green)
+/// ```
+/// #### saveTextColor
+///
+/// ```swift
+///     func saveTextColor(_ color: Color) -> some View
+/// ```
+/// When applied, replaces the color of the cell's text by the one given as parameter
 public struct SaveCell: View {
     private var action: (() -> ())
     
     @Environment(\.saveTextColor) var saveTextColor : Color
     
+    /// Default initializer
+    ///
+    /// Parameter:
+    ///  - action: method or closure property that does something when a user clicks or taps the button
+    ///
+    /// ### Usage
+    ///
+    /// ```swift
+    ///    SaveCell(action: saveContent)
+    /// ```
     public init(action: @escaping (() -> ())) {
         self.action = action
     }
